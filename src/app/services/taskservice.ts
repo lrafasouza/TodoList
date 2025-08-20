eb42a7ac7114a6ea910e61fd80c16e74239a784b
Rafael Souza <rafinha-lima1@live.com>
2025-08-20 16:41:23 -0300

    Melhorias na Service (+ Storage -MyJSON (online) )

--- src/app/services/taskservice.ts
+++ src/app/services/taskservice.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Tarefa } from '../../Model/Tarefa';
 
 @Injectable({
@@ -8,26 +8,84 @@ import { Tarefa } from '../../Model/Tarefa';
 })
 export class Taskservice {
 
-  private apiURL = 'https://my-json-server.typicode.com/lrafasouza/TodoList/tasks'
+  private apiURL = 'http://localhost:3000/tasks'
+  private storageKey = 'todolist-tasks'
 
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {
+  private isLocalStorageAvailable(): boolean {
+    return typeof(Storage) !== "undefined";
+  }
 
+  private getFromLocalStorage(): Tarefa[] {
+    if (this.isLocalStorageAvailable()) {
+      const data = localStorage.getItem(this.storageKey);
+      return data ? JSON.parse(data) : [];
+    }
+    return [];
+  }
+
+  private saveToLocalStorage(tasks: Tarefa[]): void {
+    if (this.isLocalStorageAvailable()) {
+      localStorage.setItem(this.storageKey, JSON.stringify(tasks));
+    }
   }
 
   getTasks(): Observable<Tarefa[]> {
-    return this.http.get<Tarefa[]>(this.apiURL)
+    return new Observable(observer => {
+      this.http.get<Tarefa[]>(this.apiURL).subscribe({
+        next: (tasks) => observer.next(tasks),
+        error: () => {
+          const tasks = this.getFromLocalStorage();
+          observer.next(tasks);
+        }
+      });
+    });
   }
 
   deleteTask(tarefa: Tarefa): Observable<Tarefa> {
-    return this.http.delete<Tarefa>(`${this.apiURL}/${tarefa.id}`)
+    return new Observable(observer => {
+      this.http.delete<Tarefa>(`${this.apiURL}/${tarefa.id}`).subscribe({
+        next: (result) => observer.next(result),
+        error: () => {
+          const tasks = this.getFromLocalStorage();
+          const filteredTasks = tasks.filter(t => t.id !== tarefa.id);
+          this.saveToLocalStorage(filteredTasks);
+          observer.next(tarefa);
+        }
+      });
+    });
   }
 
   updateTask(tarefa: Tarefa): Observable<Tarefa> {
-    return this.http.put<Tarefa>(`${this.apiURL}/${tarefa.id}`, tarefa)
+    return new Observable(observer => {
+      this.http.put<Tarefa>(`${this.apiURL}/${tarefa.id}`, tarefa).subscribe({
+        next: (result) => observer.next(result),
+        error: () => {
+          const tasks = this.getFromLocalStorage();
+          const index = tasks.findIndex(t => t.id === tarefa.id);
+          if (index > -1) {
+            tasks[index] = tarefa;
+            this.saveToLocalStorage(tasks);
+          }
+          observer.next(tarefa);
+        }
+      });
+    });
   }
 
   addTask(tarefa: Tarefa): Observable<Tarefa> {
-    return this.http.post<Tarefa>(`${this.apiURL}`, tarefa)
+    return new Observable(observer => {
+      this.http.post<Tarefa>(this.apiURL, tarefa).subscribe({
+        next: (result) => observer.next(result),
+        error: () => {
+          const tasks = this.getFromLocalStorage();
+          const newTask = { ...tarefa, id: Date.now() };
+          tasks.push(newTask);
+          this.saveToLocalStorage(tasks);
+          observer.next(newTask);
+        }
+      });
+    });
   }
 }
